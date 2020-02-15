81884027ce574f1fded9720792fbcf2e0530b0e4
SamuelCurrid <samuelcurrid@gmail.com>
2020-02-15 00:33:32 -0500

    Adding weather cog

--- Gompei.py
+++ Gompei.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands
 
 from Leaderboards import Leaderboards
+from Weather import Weather
 
 settings = {}
 
@@ -16,6 +17,7 @@ gompei = commands.Bot(command_prefix=get_prefix)
 
 # Extensions
 gompei.add_cog(Leaderboards(gompei))
+gompei.add_cog(Weather(gompei))
 
 # Overwrite
 gompei.remove_command("help")
